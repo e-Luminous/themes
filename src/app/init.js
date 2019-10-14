a28bda1c04a4733730e8a257059c0984e6fbdf4f
shunjid <shunjid1862@gmail.com>
2019-10-14 19:42:45 +0600

    added Dynamic Table

--- src/app/init.js
+++ src/app/init.js
@@ -4,11 +4,27 @@ $(document).ready(function () {
     $('.tooltipped').tooltip();
     $('.sidenav').sidenav();
     $('.tabs').tabs();
+    $('select').formSelect();
     
     $(".tab").on('click', function(e){
         $('#contents').hide();
         lazyLoad();
     });
+
+    /*
+     * Find the closest html table
+     * Get the content of first tr of the tbody
+     * addTBody with the reference of the tbody and first tr
+     */
+    $(".tblButtonAdder").on('click', function(e){
+        var closestTableTBody = $("#exp0"+this.id+"TBody");
+        var closestTableTBodyBaseContent = closestTableTBody.find('tr:first').html();
+        var tBodyContent = "<tr> "+ closestTableTBodyBaseContent +" </tr>";
+        var requestedTableLength = (closestTableTBody.html().match(/<tr>/g) || []).length;
+
+        addTBody(closestTableTBody,tBodyContent,requestedTableLength);
+    });
+    
 });
 
 function lazyLoad() {
@@ -17,4 +33,16 @@ function lazyLoad() {
     }).delay(1500).fadeOut(function(ex){
         $('#contents').fadeIn();
     });
+}
+
+function addTBody(tbodyID, tBodyContent, tBodyLength){
+    var tbody = '';
+
+    if(tBodyContent.includes('type="radio"')){
+        tBodyContent = tBodyContent.replace(/name="group1"/g, 'name="group'+(tBodyLength+1)+'"');
+        console.log(tBodyContent);
+    }
+
+    tbody += tBodyContent;
+    tbodyID.append(tbody);
 }
